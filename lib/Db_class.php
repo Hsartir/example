260d08ef629bb8168aa2b97c5e42e5927246f86c
Hsartir <43666437+Hsartir@users.noreply.github.com>
2018-09-28 11:22:25 +0330

    Initial commit

---
+++ lib/Db_class.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hasan
+ * Date: 27/09/2018
+ * Time: 18:34
+ */
+
+class Db{
+    private $host;
+    private $dbuser;
+    private $dbpass;
+    private $db;
+    public $table;
+    public $perfix = "ZE_";
+    public $encode = "utf8";
+
+
+    public function __construct($host,$dbuser,$dbpass,$db)
+    {
+        $this->host = $host;
+        $this->dbuser = $dbuser;
+        $this->dbpass = $dbpass;
+        $this->db = $db;
+
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        return mysqli_connect($this->host,$this->dbuser,$this->dbpass,$this->db) or die("mysql connect error!!!");
+    }
+
+    public function create($table,$sql)
+    {
+
+    }
+
+    public function insert($columns,$values)
+    {
+        $columns = is_array($columns) ? implode(",",$columns) : $columns;
+        $values = is_array($values) ? implode(",",$values) : $columns;
+        return $this->query("INSERT INTO {$this->table}({$columns}) VALUES ({$values})");
+    }
+
+    public function query($sql)
+    {
+        return mysqli_query($this->connect(),$sql);
+    }
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+        mysqli_close($this->connect());
+    }
+}
+
+$db = new Db("localhost","root","","test");
+$dbin = Db::insert(array("id","name","email","create"),array(null,"hasan","hasan@gmail.com","NOW()"));
+if($dbin){
+    echo "db insert";
+}else{
+    echo "error";
+}
---
+++ lib/User_class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hasan
+ * Date: 27/09/2018
+ * Time: 18:34
+ */
